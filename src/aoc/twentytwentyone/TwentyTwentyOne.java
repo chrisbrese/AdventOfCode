3240f5923177c6ff0cc4e02d3cb90e81af998e3d
Chris Brese <cmbrese1@gmail.com>
2021-12-07 10:00:39 -0500

    2021 Day 7

--- src/aoc/twentytwentyone/TwentyTwentyOne.java
+++ src/aoc/twentytwentyone/TwentyTwentyOne.java
@@ -625,12 +625,93 @@ public class TwentyTwentyOne extends Year {
 		
 		switch(part) {
 			case "1":
+				day7MinimumHorizontalMovement();
 				break;
 			case "2":
+				day7MinimumHorizontalMovementWithFuelDegradation();
 				break;
 			default:
+				day7MinimumHorizontalMovement();
+				day7MinimumHorizontalMovementWithFuelDegradation();
 				break;
 		}
+	}
+	
+	/**
+	 * A giant whale has decided your submarine is its next meal, and it's much faster than you are. There's nowhere to run!
+	 * Suddenly, a swarm of crabs (each in its own tiny submarine - it's too deep for them otherwise) zooms in to rescue you
+	 * They seem to be preparing to blast a hole in the ocean floor; sensors indicate a massive underground cave system just beyond where they're aiming
+	 * The crab submarines all need to be aligned before they'll have enough power to blast a large enough hole for your submarine to get through.
+	 * There's one major catch - crab submarines can only move horizontally.
+	 * You quickly make a list of the horizontal position of each crab (your puzzle input).
+	 * Crab submarines have limited fuel, so you need to find a way to make all of their horizontal positions match while requiring them to spend as little fuel as possible.
+	 * Each change of 1 step in horizontal position of a single crab costs 1 fuel.
+	 * Determine the horizontal position that the crabs can align to using the least fuel possible. How much fuel must they spend to align to that position?h.
+	 */
+	public void day7MinimumHorizontalMovement() {
+		String[] numStr = input.get(0).split(",");
+		int[] nums = new int[numStr.length];
+		
+		for(int i = 0; i < nums.length; i++) {
+			nums[i] = Integer.valueOf(numStr[i]);
+		}
+		
+		Arrays.sort(nums);
+		
+		// the median of the array should be the point that the crabs meet at - guarantees shortest distance
+		int med = nums[Math.round(nums.length/2)];
+		
+		int count = 0;
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] > med) {
+				count += (nums[i] - med);
+			}
+			else {
+				count += (med - nums[i]);
+			}
+		}
+		
+		System.out.println(CUR_YEAR + " Day 7 Part 1: " + count);
+	}
+	
+	/**
+	 * The crabs don't seem interested in your proposed solution. Perhaps you misunderstand crab engineering?
+	 * As it turns out, crab submarine engines don't burn fuel at a constant rate.
+	 * Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last: the first step costs 1, the second step costs 2, the third step costs 3, and so on.
+	 * As each crab moves, moving further becomes more expensive. This changes the best horizontal position to align them all on.
+	 * Determine the horizontal position that the crabs can align to using the least fuel possible so they can make you an escape route! How much fuel must they spend to align to that position?
+	 */
+	public void day7MinimumHorizontalMovementWithFuelDegradation() {
+		String[] numStr = input.get(0).split(",");
+		int[] nums = new int[numStr.length];
+		
+		for(int i = 0; i < nums.length; i++) {
+			nums[i] = Integer.valueOf(numStr[i]);
+		}
+		
+		// This time we use average in order to minimize the distance of the furthest away crabs
+		double sum = Arrays.stream(nums).sum();
+		// why does floor work with my input, but round(ceil) is required for the sample input?
+		double avg = Math.floor(sum/nums.length);
+		
+		int count = 0;
+		for(int i = 0; i < nums.length; i++) {
+			int tmp = 1;
+			if(nums[i] > avg) {				
+				for(int j = nums[i]; j > avg; j--) {
+					count += tmp;
+					tmp++;
+				}
+			}
+			else {
+				for(int j = nums[i]; j < avg; j++) {
+					count += tmp;
+					tmp++;
+				}
+			}
+		}
+		
+		System.out.println(CUR_YEAR + " Day 7 Part 2: " + count);
 	}
 	
 	/**
