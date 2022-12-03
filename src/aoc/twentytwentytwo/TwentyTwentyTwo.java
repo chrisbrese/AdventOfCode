e73af77478aa5ddc1171ca6740fe31dd1409a690
chris.brese <cmbrese1@gmail.com>
2022-12-03 08:07:36 -0500

    2022 day 2

--- src/aoc/twentytwentytwo/TwentyTwentyTwo.java
+++ src/aoc/twentytwentytwo/TwentyTwentyTwo.java
@@ -2,6 +2,7 @@ package aoc.twentytwentytwo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import aoc.Year;
@@ -46,6 +47,15 @@ public class TwentyTwentyTwo extends Year {
 	 * Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.
 	 * 
 	 * Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+	 * 
+	 * PART 2:
+	 * By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most
+	 * Calories of food might eventually run out of snacks.
+	 * 
+	 * To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three 
+	 * Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks, they still have two backups.
+	 * 
+	 * Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
 	 */
 	private void day1CalorieCounting() {
 		int max = 0;
@@ -93,12 +103,118 @@ public class TwentyTwentyTwo extends Year {
 		
 		switch(part) {
 			case "1":
+				day2RockPaperScissorsDirect();
 				break;
 			case "2":
+				day2RockPaperScissorsEndResult();
 				break;
 			default:
+				day2RockPaperScissorsDirect();
+				day2RockPaperScissorsEndResult();
 				break;
 		}
+	}
+	
+	/**
+	 * The Elves begin to set up camp on the beach.
+	 * To decide whose tent gets to be closest to the snack storage, a giant Rock Paper Scissors tournament is already in progress.
+	 * 
+	 * Appreciative of your help yesterday, one Elf gives you an encrypted strategy guide (your puzzle input) 
+	 * that they say will be sure to help you win.
+	 * "The first column is what your opponent is going to play: A for Rock, B for Paper, and C for Scissors. 
+	 * The second column--" Suddenly, the Elf is called away to help with someone's tent.
+	 * 
+	 * The second column, you reason, must be what you should play in response: X for Rock, Y for Paper, and Z for Scissors.
+	 * Winning every time would be suspicious, so the responses must have been carefully chosen.
+	 * 
+	 * The winner of the whole tournament is the player with the highest score.
+	 * Your total score is the sum of your scores for each round.
+	 * The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors) 
+	 * plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
+	 * 
+	 * What would your total score be if everything goes exactly according to your strategy guide?
+	 */
+	private void day2RockPaperScissorsDirect() {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		map.put("X", 1);
+		map.put("Y", 2);
+		map.put("Z", 3);
+		int totalScore = 0;
+		
+		for(String s : input) {
+			String[] battle = s.split(" ");
+			String opp = battle[0];
+			String me = battle[1];
+			
+			if(opp.equals("A") && me.equals("X") || 
+			   opp.equals("B") && me.equals("Y") ||
+			   opp.equals("C") && me.equals("Z")) {
+				totalScore += (3 + map.get(me));
+			}
+			else if (opp.equals("A") && me.equals("Z") ||
+					 opp.equals("B") && me.equals("X") ||
+					 opp.equals("C") && me.equals("Y")) {
+				totalScore += map.get(me);
+			}
+			else {
+				totalScore += (6 + map.get(me));
+			}
+		}
+		
+		System.out.println(CUR_YEAR + " Day 2 Part 1: " + totalScore);
+	}
+	
+	/**
+	 * The Elf finishes helping with the tent and sneaks back over to you.
+	 * "Anyway, the second column says how the round needs to end: X means you need to lose, 
+	 * Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+	 * 
+	 * The total score is still calculated in the same way, but now you need to figure out what 
+	 * shape to choose so the round ends as indicated.
+	 * 
+	 * Following the Elf's instructions for the second column, what would your total score be 
+	 * if everything goes exactly according to your strategy guide?
+	 */
+	private void day2RockPaperScissorsEndResult() {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		map.put("A", 1);
+		map.put("B", 2);
+		map.put("C", 3);
+		int totalScore = 0;
+		
+		for(String s : input) {
+			String[] battle = s.split(" ");
+			String opp = battle[0];
+			String me = battle[1];
+			
+			if(me.equals("Y")) {
+				totalScore += (3 + map.get(opp));
+			}
+			else if (me.equals("X")) {
+				if(opp.equals("A")) {
+					totalScore += map.get("C");
+				}
+				else if(opp.equals("B")) {
+					totalScore += map.get("A");
+				}
+				else if(opp.equals("C")) {
+					totalScore += map.get("B");
+				}
+			}
+			else {
+				if(opp.equals("A")) {
+					totalScore += (6 + map.get("B"));
+				}
+				else if(opp.equals("B")) {
+					totalScore += (6 + map.get("C"));
+				}
+				else if(opp.equals("C")) {
+					totalScore += (6 + map.get("A"));
+				}
+			}
+		}
+		
+		System.out.println(CUR_YEAR + " Day 2 Part 2: " + totalScore);
 	}
 	
 	/**
